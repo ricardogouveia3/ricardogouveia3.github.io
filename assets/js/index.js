55e1d06db15986f6afdaed8885c0bb88fc0056d6
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2017-12-05 05:42:28 -0300

    Portfolio modal and grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,40 @@ function dropdownClose() {
   menuList.style.display = "none";
 }
 
+/* Portfolio Show&Hide */
+var modal = document.getElementById("portfolioModal");
+var menuAbout = document.getElementById("menuAbout");
+var menuPortfolio = document.getElementById("menuPortfolio");
+
+function openPortfolio() {
+  menuAbout.classList.remove("active");
+  menuPortfolio.classList.add("active");
+  modal.style.transform = "translate(0)";
+}
+
+function closePortfolio() {
+  modal.style.transform = "translate(120%)";
+  menuPortfolio.classList.remove("active");
+  menuAbout.classList.add("active");
+}
+
+var menuAboutDrop = document.getElementById("menuAboutDrop");
+var menuPortfolioDrop = document.getElementById("menuPortfolioDrop");
+
+function openPortfolioDrop() {
+  menuAboutDrop.classList.remove("active");
+  menuPortfolioDrop.classList.add("active");
+  modal.style.transform = "translate(0)";
+  dropdownClose();
+}
+
+function closePortfolioDrop() {
+  modal.style.transform = "translate(0, -200%)";
+  menuPortfolioDrop.classList.remove("active");
+  menuAboutDrop.classList.add("active");
+  dropdownClose();
+}
+
 /* My actual age */
 function calcAge(dateString) {
   var birthday = +new Date(dateString);
