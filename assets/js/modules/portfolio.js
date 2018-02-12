874bd7c795fc9fa1c95d394b01535e1d85cdbd36
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2018-02-12 11:29:20 -0300

    Add min height to portfolio links. Safari/IE still broken

--- assets/js/modules/portfolio.js
+++ assets/js/modules/portfolio.js
@@ -3,17 +3,20 @@ var modal = document.getElementById("portfolioModal");
 var menuAbout = document.getElementById("menuAbout");
 var menuPortfolio = document.getElementById("menuPortfolio");
 
+var portfolioState = false;
+
 function openPortfolio() {
+  portfolioState = true;
   menuAbout.classList.remove("menu__list__item__link_active");
   menuPortfolio.classList.add("menu__list__item__link_active");
   modal.style.display = "grid";
   setTimeout(function(){
     modal.style.transform = "translate(0)";
   }, 100);
-
 }
 
 function closePortfolio() {
+  portfolioState = false;
   modal.style.transform = "translate(120%)";
   menuPortfolio.classList.remove("menu__list__item__link_active");
   menuAbout.classList.add("menu__list__item__link_active");
