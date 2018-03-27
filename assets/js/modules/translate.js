2be1dcb03c6fb02600acf9eaa812b05b45ab5293
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2018-03-27 06:18:03 -0300

    More lang fluid text and canonical link for https seo

---
+++ assets/js/modules/translate.js
@@ -0,0 +1,26 @@
+/*
+-TRANSLATE (duplicate and switch display)(.pt_br & .en_us)
+meta:
+  html lang
+  meta description content
+
+social-meta:
+  twitter:description content
+  og:description content
+
+nav
+  sobre
+  contato
+
+main-text
+  p1
+  p2
+  p3
+
+project
+  title
+  service
+
+-ADD
+  select lang link
+*/
