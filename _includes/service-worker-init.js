5d5f615339798f5c188af5b35b4cfc2e321c33a9
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2018-03-31 06:24:24 -0300

    Remove service worker and pwa stuff for future implementation

--- _includes/service-worker-init.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then(reg => console.info('registered sw', reg))
-    .catch(err => console.error('error registering sw', err));
-}
