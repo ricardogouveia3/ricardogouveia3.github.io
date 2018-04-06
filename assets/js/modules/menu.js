bf1ea14fb0d8af1683f009df2d05d7e4747aa2b3
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2018-04-06 08:47:49 -0300

    Fix menu transform on desktop

--- assets/js/modules/menu.js
+++ assets/js/modules/menu.js
@@ -1,28 +1,63 @@
+/*                       */
 /* Mobile menu Show&Hide */
+/*                       */
+
+/* Get HTML elements */
 var divCloseMenu = document.getElementById("divCloseMenu");
 var itemCloseMenu = document.getElementById("itemCloseMenu");
 var menu = document.getElementById("menu");
 var menuIcon = document.getElementById("menuButton");
 
-var menuState = false;
+/* Get viewport props */
+var windowWidth = window.innerWidth;  //  Get viewport width
 
+/* Set elements status variables n default values */
+var menuState = false;  // Is menu open?
+var mobileMenu = false; // Is menu on mobile mode?
+
+
+/* Checks for every window rezise */
+window.onresize = function() {  //  Updates viewport width on resize
+  windowWidth = window.innerWidth;
+
+  if (windowWidth <= 600) { mobileMenu = true; } else { mobileMenu = false; } // Sets variable to watch menu mode
+  if (mobileMenu == false) {
+    menu.style.transform = "translate(0)";  // Resets desktop menu translate on resize;
+  } else { 
+    menu.style.transform = "translate(120%)";
+    divCloseMenu.style.display = "none";
+  }  // Sets mobile menu to hide position on resize
+};
+
+
+/* Open menu func */
 function openMenu() {
   var menuState = true;
-  divCloseMenu.style.display = "block";
-  setTimeout(function(){
-    menu.style.transform = "translate(0)";
-  }, 100);
+  
+  if (mobileMenu == true) {
+    divCloseMenu.style.display = "block";
+    setTimeout(function(){
+      menu.style.transform = "translate(0)";  // Move menu to inside viewport
+    }, 100);
+  }
 }
 
+/* Open menu event listeners */
 menuIcon.addEventListener("click", openMenu, false);
 
+
+/* Close menu func */
 function closeMenu() {
   var menuState = false;
   divCloseMenu.style.display = "none";
-  setTimeout(function(){
-    menu.style.transform = "translate(120%)";
-  }, 100);
+
+  if (mobileMenu == true) {
+    setTimeout(function(){
+      menu.style.transform = "translate(120%)"; // Move menu to outside viewport
+    }, 100);
+  }
 }
 
+/* Close menu event listeners */
 divCloseMenu.addEventListener("click", closeMenu, false);
 itemCloseMenu.addEventListener("click", closeMenu, false);
