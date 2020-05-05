ffc60675ce178ed76eb2943e85ae6d231dd1353c
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2020-05-05 15:32:27 -0300

    Remove axios

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -52,7 +52,7 @@ export default {
       return response;
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import axios from "axios";
 import VueHead from "vue-head";
 
 Vue.use(VueHead);
 Vue.config.productionTip = false;
-Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App)
