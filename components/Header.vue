f9ac35657c7f9e0163042a3f317fff1338f60f5b
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2021-04-25 07:59:41 -0300

    Unify header menu responsive

--- components/Header.vue
+++ components/Header.vue
@@ -1,35 +1,19 @@
 <template>
   <header>
     <div class="DEF_wrapper">
-      <nav class="nav--desktop">
+      <nav class="nav">
         <ul>
+          <li class="mobile-logo">
+            <a href="/"><img class="logo" src="../assets/img/logo.svg" /></a>
+          </li>
           <li>
             <a id="menuAbout" href="#about" data-scroll>About</a>
           </li>
           <li>
             <a id="menuPortfolio" href="#portfolio" data-scroll>Portfolio</a>
           </li>
-          <li>
+          <li class="desktop-logo">
             <a href="/"><img class="logo" src="../assets/img/logo.svg" /></a>
-          </li>
-          <li>
-            <a id="menuPosts" href="#posts" data-scroll>Posts</a>
-          </li>
-          <li>
-            <a id="menuContact" href="#contact" data-scroll>Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <nav class="nav--mobile">
-        <ul>
-          <li>
-            <a href="/"><img class="logo" src="../assets/img/logo.svg" /></a>
-          </li>
-          <li>
-            <a id="menuAbout" href="#about" data-scroll>About</a>
-          </li>
-          <li>
-            <a id="menuPortfolio" href="#portfolio" data-scroll>Projects</a>
           </li>
           <li>
             <a id="menuPosts" href="#posts" data-scroll>Posts</a>
@@ -56,39 +40,9 @@ export default {
 header {
   width: 100%;
 }
-.nav--desktop {
-  padding: 0 20px;
-  @media (max-width: 700px) {
-    display: none;
-  }
-}
-ul {
-  list-style-type: none;
-  display: flex;
-  justify-content: space-between;
-  margin: 50px 0;
-  padding: 0;
-}
-li {
-  margin: 0;
-  padding: 0;
-  display: flex;
-  align-items: center;
-}
-li a {
-  color: $armadillo;
-  font-size: 20px;
-  line-height: 25px;
-  text-transform: uppercase;
-  transition: color 0.3s ease-in-out;
-  letter-spacing: 1px;
 
-  &:hover {
-    color: $caribbean-green;
-  }
-}
-.nav--mobile {
-  @media (min-width: 700px) {
+.nav {
+  .desktop-logo {
     display: none;
   }
 
@@ -97,30 +51,73 @@ li a {
     justify-content: center;
     align-items: center;
     text-align: center;
+    padding: 20px 0;
+    margin: 35px 0 20px 0;
   }
 
   li {
-    max-width: 200px;
-    display: block;
-    width: 100%;
-    padding: 1ch 0;
-    border-bottom: 1px solid rgba($armadillo, 0.5);
+    display: flex;
+    justify-content: center;
+    margin: 0;
+    padding: 0 0 1ch 0;
 
     &:hover {
       border-color: rgba($caribbean-green, 0.5);
     }
-    &:first-of-type {
-      margin-bottom: 30px;
-      border: 0;
-    }
+
     a {
+      border-bottom: 1px solid rgba($armadillo, 0.5);
+      padding-bottom: 10px;
       display: block;
-      width: 100%;
+      width: 200px;
       text-decoration: none;
       text-align: center;
+      color: $armadillo;
+      line-height: 25px;
+      text-transform: uppercase;
+      transition: color 0.3s ease-in-out;
+      letter-spacing: 1px;
+
+      &:hover {
+        color: $caribbean-green;
+      }
+    }
+
+    &.mobile-logo a {
+      border-bottom: none;
     }
   }
 }
+
+@media screen and (min-width: 700px) {
+  .nav {
+    padding: 0 20px;
+
+    .mobile-logo {
+      display: none;
+    }
+
+    .desktop-logo {
+      display: block;
+    }
+
+    ul {
+      list-style-type: none;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      margin: 50px 0 25px 0;
+      padding: 0;
+    }
+
+    li a {
+      width: unset;
+      font-size: 20px;
+      border-bottom: none;
+    }
+  }
+}
+
 hr {
   border: 0;
   margin-bottom: 150px;
