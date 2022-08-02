5d421c4003c94882b75eb5d72e23b038a986bda5
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2022-08-02 07:35:18 -0300

    Change portfolio for new api url

--- api/portfolio.js
+++ api/portfolio.js
@@ -1,22 +1,38 @@
 import { server } from '../config'
 
-const API_PORTFOLIO_ENDPOINT = 'https://api.rcrd.dev/portfolio'
+const API_PORTFOLIO_ENDPOINT = 'https://rcrd-api.azurewebsites.net/portfolio'
 const BACKUP_PORTFOLIO_DATA = `${server}/data/backup-portfolio.json`
+
+const checkPortfolioResponse = (responseStatus) => {
+  if (responseStatus.fromBackup) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      "Api is returning it's local back-up data. Error: " + responseStatus.error
+    )
+  }
+}
 
 export const Portfolio = {
   getData: async () => {
-    return await fetch(API_PORTFOLIO_ENDPOINT)
-      .then((res) => res.json())
-      .catch(async function () {
-        // eslint-disable-next-line no-console
-        console.error(
-          'Unable to fetch portfolio data from API. Using backup JSON'
-        )
-        const localBackup = await fetch(BACKUP_PORTFOLIO_DATA)
-        const parsedLocalBackup = await localBackup.json()
+    try {
+      let response = await fetch(API_PORTFOLIO_ENDPOINT)
+      response = await response.json()
 
-        return parsedLocalBackup
-      })
+      checkPortfolioResponse(response.status)
+
+      return await response.data
+    } catch (error) {
+      let response = await fetch(BACKUP_PORTFOLIO_DATA)
+      response = response.json()
+
+      // eslint-disable-next-line no-console
+      console.warn(
+        'Unable to fetch portfolio data from API. Using back-up JSON data. Error: ' +
+          error
+      )
+
+      return response
+    }
   },
 }
 
